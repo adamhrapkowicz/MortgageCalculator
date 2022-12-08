fb1e8fb77bde8531c3b9d137923b86f9670fbff7
wolsz <wolsz@DESKTOP-7FLJELI>
2022-12-08 17:39:54 +0000

    Add project files.

---
+++ MortgageCalculator/MortgageCalculator.cs
@@ -0,0 +1,18 @@
+﻿public class MortgageCalculator
+{
+    internal object CalculateCost(double price, double annualIntrest, double numberOfYears)
+    {
+        double cost = 0;
+        var remainingAmount = price;
+        var monthlyIntrest = annualIntrest / 100 /12;
+        var numberOfMonths = numberOfYears * 12;
+        var monthlyPayment = price / numberOfMonths;
+
+        for (var i = 0; i < numberOfMonths; i++)
+        {
+            cost += remainingAmount * monthlyIntrest;
+            remainingAmount -= monthlyPayment;
+        }
+        return cost;
+    }
+}
---
+++ MortgageCalculator/Program.cs
@@ -0,0 +1,64 @@
+﻿
+double price = 0.0;
+double annualIntrest;
+double numberOfYears;
+
+while (price < 10000)
+{
+    price = GetPriceFromUser();
+    if (price >= 10000)
+        break;
+    Console.WriteLine($"The minimum value of mortgage is £10000");
+}
+
+do
+{
+    annualIntrest = GetAnnualIntrestFromUser();
+    if (annualIntrest < 0.01 || annualIntrest > 50)
+    {
+        Console.WriteLine($"The intrest rate must be between 0.01 and 50%");
+    }
+} while (annualIntrest < 0.01 || annualIntrest > 50);
+
+do
+{
+    numberOfYears = GetNumberOfYearsFromUsers();
+    if (numberOfYears < 1 || numberOfYears >30)
+    {
+        Console.WriteLine($"The lenght of mortgage must be between 1 and 30 years");
+    }
+} while (numberOfYears < 1 || numberOfYears >30);
+
+var mortgageCalculator = new MortgageCalculator();
+
+var cost = mortgageCalculator.CalculateCost(price, annualIntrest,numberOfYears);
+
+Console.WriteLine($"The cost of your mortgage is £{cost:N2} on top of what you borrow.");
+
+double GetNumberOfYearsFromUsers()
+{
+    return GetDoubleFromUser("Enter the lenght of your mortgage in years.");
+}
+
+double GetAnnualIntrestFromUser()
+{
+    return GetDoubleFromUser("Enter the intrest rate in %.");
+}
+
+double GetPriceFromUser()
+{
+    return GetDoubleFromUser("Enter the price of the house in £.");
+}
+
+double GetDoubleFromUser(string messageForUser)
+{
+    var validInput = false;
+    double result = 0.0;
+    while(!validInput)
+    {
+        Console.WriteLine(messageForUser);
+        var input = Console.ReadLine();
+        validInput = double.TryParse(input, out result);
+    }
+    return result;
+}
